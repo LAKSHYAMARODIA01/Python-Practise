e07802ae8166e4d09c57a27ed70dc80c802f5138
LAKSHYAMARODIA01 <lakshyamarodia@gmail.com>
2024-09-15 21:51:28 +0530

    Add Chapter2-ps

---
+++ Chapter2-ps/problem2.py
@@ -0,0 +1,6 @@
+# WAP to find remainder when a number is divided by z.
+a = 37
+
+b = 5
+
+print("Remainder when a is divided by b is", a % b)
---
+++ Chapter2-ps/problem4.py
@@ -0,0 +1,7 @@
+# use comparision operator to find out whether "a" given variable a is greater than 'b' or not.Take a=34 and b=80.Tab.
+
+a = int(input("Enter number 1: "))
+b = int(input("Enter number 2: "))
+
+print("a is greater than b is", a>b)
+
