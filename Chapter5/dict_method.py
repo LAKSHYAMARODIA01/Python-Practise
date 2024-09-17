baf3f0e9e684f2c3bc48f5fc0cf2caad27f5bc5b
LAKSHYAMARODIA01 <lakshyamarodia@gmail.com>
2024-09-17 16:03:57 +0530

    Add Chapter5

---
+++ Chapter5/dict_method.py
@@ -0,0 +1,15 @@
+marks = {
+    "Darry": 100,
+    "Shubham": 56,
+    "Rohan": 23,
+    0: "Darry"
+}
+
+# print(marks.items())
+# print(marks.keys())
+# print(marks.values())
+# marks.update({"Harry": 99, "Renuka": 100})
+# print(marks)
+
+print(marks.get("Darry2")) # Prints None
+print(marks["Darry2"]) # Returns an error
